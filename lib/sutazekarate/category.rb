abb940a3edf93aafae4a66cfd38fd80bab5dd979
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2025-02-20 09:43:26 +0100

    Fix ladder pool parsing

--- lib/sutazekarate/category.rb
+++ lib/sutazekarate/category.rb
@@ -175,7 +175,7 @@ module Sutazekarate
 
         stage_index = 0
         loop do
-          stage_element = html.search(".stlpecn.posun#{stage_index}").first
+          stage_element = pool.search(".stlpecn.posun#{stage_index}").first
           unless stage_element
             break
           end
--- lib/sutazekarate/version.rb
+++ lib/sutazekarate/version.rb
@@ -1,3 +1,3 @@
 module Sutazekarate
-  VERSION = '0.0.13'.freeze
+  VERSION = '0.0.14'.freeze
 end
